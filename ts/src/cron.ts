f17d2fdaeb06856783294f35f5874f5376087a26
FUJIKAKE Takayoshi <fujikake.takayoshi@gmail.com>
2026-05-03 21:59:43 +0900

    nodecronの並び順を見直し

--- ts/src/cron.ts
+++ ts/src/cron.ts
@@ -2,16 +2,16 @@ import cron from 'node-cron';
 import { prisma } from './config.ts';
 import { runJob, scrapeOkwaveUrls, setQaTitleBodyFiltering } from './jobs.ts';
 
-// 毎分：詳細取得
-cron.schedule('* * * * *', async () => {
-	console.log('[cron] setQaTitleBodyFiltering start');
-	await setQaTitleBodyFiltering();
-});
-
 // 15分に1回：URL収集
 cron.schedule('*/15 * * * *', async () => {
 	console.log('[cron] runJob start');
 	await runJob('OKWAVE', scrapeOkwaveUrls);
+});
+
+// 毎分：質問のタイトル本文取得
+cron.schedule('* * * * *', async () => {
+	console.log('[cron] setQaTitleBodyFiltering start');
+	await setQaTitleBodyFiltering();
 });
 
 // プロセス終了時にDB切断
